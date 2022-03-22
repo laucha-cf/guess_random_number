1f52544d128d62c720c74adb8c15a148aa2ef820
Lautaro Perez Carmona <lautaropc2001@gmail.com>
2022-03-22 11:08:21 -0300

    Now you can be the guesser

--- aux_functions.py
+++ aux_functions.py
@@ -1,5 +1,7 @@
 #The user choose a number until he hit the guess
 
+import random
+
 def user_guess(rand_numb):
     guess_numb = 0
     while (guess_numb != rand_numb ):
@@ -10,3 +12,19 @@ def user_guess(rand_numb):
             print('Too low!')
     
     print(f'That is it! You guessed the number >> {guess_numb}')
+
+def computer_guess( top ):
+    bottom = 1
+    feedback = ''
+    while feedback != 'c':
+        pc_guess = random.randint( bottom, top )
+        feedback = input(f'Is {pc_guess} too High(H), too Low(L) or correct(C): ').lower()
+        if feedback != 'c':
+            if feedback == 'h':
+                top = pc_guess
+            elif feedback == 'l':
+                bottom = pc_guess
+            print(f'BETWEEN {bottom} and {top}')
+            
+    print(f'You guessed, genius. The number is {pc_guess}. Anyway, you are a computer')
+    
